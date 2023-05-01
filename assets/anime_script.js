0c37779d40a64ec61e71fd7a3b56c7a36fd8b4fa
Jlevbury <125767916+Jlevbury@users.noreply.github.com>
2023-05-01 15:15:34 -0400

    fan favorites query set to autoload and populate the html element

--- assets/anime_script.js
+++ assets/anime_script.js
@@ -8,7 +8,7 @@ const headers = new Headers({
 // Get the submit button element
 var animeBtn = document.getElementById("animeBtn");
 
-// Add a click event listener to the submit button
+
 // Add a click event listener to the submit button
 animeBtn.addEventListener("click", async function () {
 	// Get the select element and its selected options
@@ -87,4 +87,45 @@ async function fetchAnimeData(selectedValues, apiUrl, headers) {
 	}
   }
 
+  const settings = {
+	async: true,
+	crossDomain: true,
+	url: 'https://myanimelist.p.rapidapi.com/anime/top/bypopularity',
+	method: 'GET',
+	headers: {
+	  'X-RapidAPI-Key': '35e91351f9msh6c0ad19b1337776p1fab34jsn871f70676154',
+	  'X-RapidAPI-Host': 'myanimelist.p.rapidapi.com'
+	}
+  };
+  
+  $.ajax(settings).done(function (response) {
+	// Return only the first 5 items of the response array
+	const top5Anime = response.slice(0, 5);
+	console.log(top5Anime);
+	// Get the results div element
+	const popularDiv = document.getElementById("popular");
+	// Clear previous results
+	popularDiv.innerHTML = '';
+
+	// Loop through the animeData array and create elements for each item
+	top5Anime.forEach((item) => {
+		// Create a container div element for the item
+		const itemDiv = document.createElement("div");
+		itemDiv.className = "anime-item";
+	
+		// Set the content of the item div with separate elements for each property
+		itemDiv.innerHTML = `
+		  <h3>${item.title}</h3>
+		  <img src="${item.picture_url}" alt="${item.title}">
+		  <p>Rank: ${item.rank}</p>
+		  <p>Type: ${item.type}</p>
+		  <p>Aired on: ${item.aired_on}</p>
+		`;
+	
+		// Append the item div to the results div
+		popularDiv.appendChild(itemDiv);
+	  });
+
+
+  });
   
